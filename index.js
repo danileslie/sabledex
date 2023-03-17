0380d69bb05a764c9795d94408e20c0630ab1c69
Dani <s.leslie94@gmail.com>
2023-03-17 15:02:12 -0400

    display information

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-// fetch(url)
-// .then(res => res.json()) // parse response as JSON
-// .then(data => {
-//   console.log(data);
-// })
-// .catch(err => {
-//     console.log(`error ${err}`)
-// });
+let getPokemon = document.querySelector('#getpokemon');
+let pokemonName = document.querySelector('#pokemonName');
+let pokemonType1= document.querySelector('#pokemonType1');
+let pokemonType2=document.querySelector('#pokemonType2');
+let pokemonImage = document.querySelector('#pokemonImage');
 
-console.log('loaded')
+
+
+
+getPokemon.addEventListener('click', pokemonInformation)
+
+function pokemonInformation(){
+    
+    let pokemonId = document.querySelector('#pokemonId').value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data);
+  pokemonName.textContent = data.name;
+  pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+  pokemonType1.textContent = data.types[0].type.name;
+  if (!data.types[1]){
+    clearData();
+  } else{
+    pokemonType2.textContent = data.types[1].type.name;
+  }
+  
+
+
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
+}
+
+function clearData(){
+    pokemonType2.textContent='';
+}
