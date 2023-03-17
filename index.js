2cca5c492a223c7cf6aa07c31c0ea1570fc42b77
Dani <s.leslie94@gmail.com>
2023-03-17 16:40:14 -0400

    change input, capitalize name, adjust colours

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let getPokemon = document.querySelector('#getpokemon');
+let getPokemon = document.querySelector('#getPokemon');
 let pokemonName = document.querySelector('#pokemonName');
 let pokemonType1 = document.querySelector('#pokemonType1');
 let pokemonType2 = document.querySelector('#pokemonType2');
@@ -10,26 +10,28 @@ let pokemonImage = document.querySelector('#pokemonImage');
 getPokemon.addEventListener('click', pokemonInformation)
 
 function pokemonInformation() {
-
     let pokemonId = document.querySelector('#pokemonId').value.toLowerCase();
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
             console.log(data);
-            pokemonName.textContent = data.name;
+            let letterUppercase = data.name.charAt(0).toUpperCase();
+            let remainingName = data.name.slice(1);
+            let nameUppercase = letterUppercase + remainingName;
+            pokemonName.textContent = nameUppercase;
             pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
 
-            if (!data.types[1]) {
-                pokemonType2.style.display = 'none';
-            } else {
-                pokemonType1.textContent = data.types[0].type.name;
-                pokemonType1.className = `${data.types[0].type.name}`
+            pokemonType1.textContent = data.types[0].type.name;
+            pokemonType1.className = data.types[0].type.name;
+
+            if (data.types[1]) {
+                pokemonType2.style.display='block';
                 pokemonType2.textContent = data.types[1].type.name;
                 pokemonType2.className = `${data.types[1].type.name}`
+               
+            } else {
+                pokemonType2.style.display='none'; 
             }
-
-
-
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -38,4 +40,5 @@ function pokemonInformation() {
 
 function clearData() {
     pokemonType2.textContent = '';
+    pokemonType2.style.display='none';
 }
