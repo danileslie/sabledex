f5091c77ef84069de8f3b348b24d273e0bb0fd32
Dani <s.leslie94@gmail.com>
2023-03-17 15:42:16 -0400

    add type colours and edit container

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let getPokemon = document.querySelector('#getpokemon');
 let pokemonName = document.querySelector('#pokemonName');
-let pokemonType1= document.querySelector('#pokemonType1');
-let pokemonType2=document.querySelector('#pokemonType2');
+let pokemonType1 = document.querySelector('#pokemonType1');
+let pokemonType2 = document.querySelector('#pokemonType2');
 let pokemonImage = document.querySelector('#pokemonImage');
 
 
@@ -9,30 +9,33 @@ let pokemonImage = document.querySelector('#pokemonImage');
 
 getPokemon.addEventListener('click', pokemonInformation)
 
-function pokemonInformation(){
-    
+function pokemonInformation() {
+
     let pokemonId = document.querySelector('#pokemonId').value.toLowerCase();
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-.then(res => res.json()) // parse response as JSON
-.then(data => {
-  console.log(data);
-  pokemonName.textContent = data.name;
-  pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
-  pokemonType1.textContent = data.types[0].type.name;
-  if (!data.types[1]){
-    clearData();
-  } else{
-    pokemonType2.textContent = data.types[1].type.name;
-  }
-  
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data);
+            pokemonName.textContent = data.name;
+            pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+
+            if (!data.types[1]) {
+                pokemonType2.style.display = 'none';
+            } else {
+                pokemonType1.textContent = data.types[0].type.name;
+                pokemonType1.className = `${data.types[0].type.name}`
+                pokemonType2.textContent = data.types[1].type.name;
+                pokemonType2.className = `${data.types[1].type.name}`
+            }
 
 
-})
-.catch(err => {
-    console.log(`error ${err}`)
-});
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
 }
 
-function clearData(){
-    pokemonType2.textContent='';
+function clearData() {
+    pokemonType2.textContent = '';
 }
