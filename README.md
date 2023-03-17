1f2366dd6d139b6009679208d1e0e5b3b50f9151
Dani <s.leslie94@gmail.com>
2023-03-17 14:03:48 -0400

    add js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sabledex
+
+A Pokédex, that can be used to pull up information on any Pokémon by name. Displays the name, picture, and type(s). 
---
+++ index.js
@@ -0,0 +1,10 @@
+// fetch(url)
+// .then(res => res.json()) // parse response as JSON
+// .then(data => {
+//   console.log(data);
+// })
+// .catch(err => {
+//     console.log(`error ${err}`)
+// });
+
+console.log('loaded')
